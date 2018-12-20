388ab00beb9528f772d300caaa83c6bb9c64bfa5
Glitch (cross-me) <none>
2018-12-20 16:40:45 +0000

    🌪🎺 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+  
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+  });
+});
+
+// listen for requests :)
+http.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + http.address().port);
+});
