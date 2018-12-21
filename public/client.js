84830aa4656d848551808417a3be965aeae1a490
crossjames934 <crossjames934@gmail.com>
2018-12-21 17:06:17 +0000

    bullets disppear and temporary invicibility upon being hit

--- public/client.js
+++ public/client.js
@@ -3,31 +3,52 @@ let name = "";
 let id = "";
 
 $(function () {
-  $.get("/getID", function(data) {
-    console.log(data);
-    id = data;
-  });
+    $.get("/getID", function (data) {
+        id = data;
+    });
 
-  $('#submitName').submit(function(e) {
-    name = $("#userNameIn").val();
-    $('#enterName').hide();
-    e.preventDefault();
-  });
-  
-  $('#chatForm').submit(function(){
-    socket.emit('chat message', name + ": " + $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  
-  socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
-    console.log($('#msgContainer')[0].scrollHeight);
-    $('#msgContainer').scrollTop($('#msgContainer')[0].scrollHeight);
-  });
-  
-  socket.on('player coordinates', function(coords) {
-    let incoming = JSON.parse(coords);
-    opponents = incoming.data;
-  });
+    $('#submitName').submit(function (e) {
+        name = $("#userNameIn").val();
+        $('#enterName').hide();
+        e.preventDefault();
+    });
+
+    $('#chatForm').submit(function () {
+        const m = $('#m');
+        socket.emit('chat message', name + ": " + m.val());
+        m.val('');
+        return false;
+    });
+
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+        const container = $('#msgContainer');
+        container.scrollTop(container[0].scrollHeight);
+    });
+
+    socket.on('splice bullet', function (info) {
+        let parsedInfo = JSON.parse(info);
+        let opponentId = parsedInfo.id;
+        let bulletIndex = parsedInfo.bulletIndex;
+
+
+        // if (player.id === opponentId) {
+        //   console.log("delete all bullets of player");
+        //   player.bullets = [];
+        //   // player.bullets.splice(bulletIndex, 1);
+        // }
+        for (let i = 0; i < opponents.length; i++) {
+            if (opponents[i].id === opponentId) {
+                // opponents[i].bullets = [];
+                player.bullets.splice(bulletIndex, 1);
+                // opponents[i].bullets.splice(bulletIndex, 1);
+                break;
+            }
+        }
+    });
+
+    socket.on('player coordinates', function (coords) {
+        let incoming = JSON.parse(coords);
+        opponents = incoming.data;
+    });
 });
--- server.js
+++ server.js
@@ -10,48 +10,53 @@ let players = [];
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/getID', function(req, res) {
-  res.send(shortid.generate());
+app.get('/getID', function (req, res) {
+    res.send(shortid.generate());
 });
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
-  
-  socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
-  });
-  
-  socket.on('player coordinates', function(coords) {
-    // io.emit('player coordinates', coords);
-    let coordsObject = JSON.parse(coords);
-    let matches = 0;
-    for (let i = 0; i < players.length; i++) {
-      if (players[i].id === coordsObject.id) {
-        players[i].x = coordsObject.x;
-        players[i].y = coordsObject.y;
-        players[i].bullets = coordsObject.bullets;
-        players[i].dir = coordsObject.dir;
-        matches++;
-      } 
-    }
-    if (matches === 0 && coordsObject.id.length > 0) {
-      players.push(coordsObject);
-    }
-    let outData = {data: players};
-    io.emit('player coordinates', JSON.stringify(outData));
-  });
-  
-  socket.on('disconnect', function() {
-    players = [];
-    console.log('a user disconnected');
-  });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('player coordinates', function (coords) {
+        // io.emit('player coordinates', coords);
+        let coordsObject = JSON.parse(coords);
+        let matches = 0;
+        for (let i = 0; i < players.length; i++) {
+            if (players[i].id === coordsObject.id) {
+                players[i].x = coordsObject.x;
+                players[i].y = coordsObject.y;
+                players[i].bullets = coordsObject.bullets;
+                players[i].dir = coordsObject.dir;
+                players[i].invincible = coordsObject.invincible;
+                matches++;
+            }
+        }
+        if (matches === 0 && coordsObject.id.length > 0) {
+            players.push(coordsObject);
+        }
+        let outData = {data: players};
+        io.emit('player coordinates', JSON.stringify(outData));
+    });
+
+    socket.on('splice bullet', function (info) {
+        io.emit('splice bullet', info);
+    });
+
+    socket.on('disconnect', function () {
+        players = [];
+        console.log('a user disconnected');
+    });
 });
 
 // listen for requests :)
-http.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + http.address().port);
+http.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + http.address().port);
 });
